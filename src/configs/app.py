477051a53dbb495f36693a35ec3902cddf2c2c0d
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2026-05-10 14:45:54 +0300

    [add] env  SERVICE_INTERNAL_PORT

--- src/configs/app.py
+++ src/configs/app.py
@@ -1,9 +1,19 @@
 from typing import List
 
+from marshmallow.validate import OneOf
+
 from configs.env import env
+
 
 DEBUG = env.bool("DEBUG", False)
 
+MODE = env.str(
+    "MODE",
+    validate=OneOf(
+        ("worker", "manager",),
+        error="MODE must be one of: {choices}"
+    )
+)
 # BACKEND_SECRET_KEY = env.str("BACKEND_SECRET_KEY")
 # BACKEND_ALLOWED_HOSTS: List[str] = env.list("BACKEND_ALLOWED_HOSTS")
 # SERVER_BACKEND_URL: str = env.str("SERVER_BACKEND_URL")
